3b2d237aa2cee7dd0dfce7a3b22c85363d608040
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-08-18 14:59:55 -0300

    Update Exer06.java

--- src/com/loaine/cursojava/aula20/labs/Exer06.java
+++ src/com/loaine/cursojava/aula20/labs/Exer06.java
@@ -8,102 +8,92 @@ public class Exer06 {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		char[][] jogoDaVelha = new char[3][3];
+		char[][] jogoVelha = new char[3][3];
 		
 		System.out.println("Jogador 1 = X");
 		System.out.println("Jogador 2 = O");
 		
 		boolean ganhou = false;
-		int jogada = 1;
+		int jogadas = 1;
 		char sinal;
 		int linha = 0, coluna = 0;
 		
 		while(!ganhou) {
-
-			if (jogada % 2 == 1) {
-				sinal = 'X';
+			
+			if(jogadas % 2 == 1) { //jogador 1
 				System.out.println("Vez do jogador 1. Escolha linha e coluna (1-3).");
-				
+				sinal = 'X';
 			}else {
+				System.out.println("Vez do jogador 2. Escolha linha e coluna (1-3).	");
 				sinal = 'O';
-				System.out.println("Vez do jogador 2. Escolha linha e coluna (1-3).");
-				
 			}
 			
 			boolean linhaValida = false;
 			
 			while(!linhaValida) {
-				
-				System.out.println("Entre com a linha (1, 2 ou 3)");
+				System.out.println("Entre com a linha (1, 2 ou 3):");
 				linha = scan.nextInt();
 				
-				if (linha >= 1 && linha <= 3) {
-					linhaValida = true;
+				if (linha >=1 && linha <= 3) {
+					linhaValida = true;	
 				}else {
-					System.out.println("Linha inválida, tente novamente.");
+					System.out.println("Entrada inválida, tente novamente.");
 				}
 			}
 			
 			boolean colunaValida = false;
-			
+
 			while (!colunaValida) {
-				System.out.println("Entre com a coluna (1, 2 ou 3)");
+				System.out.println("Entre com a coluna (1-3):");
 				coluna = scan.nextInt();
 				
 				if (coluna >= 1 && coluna <= 3) {
 					colunaValida = true;
-					
 				}else {
-					System.out.println("Coluna inválida, tente novamente.");
+					System.out.println("Entrada inválida, tente novamente.");
 				}
 			}
 			
 			linha--;
 			coluna--;
-			
-			if (jogoDaVelha[linha][coluna] == 'X' || jogoDaVelha[linha][coluna] == 'O') {
+			if(jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O') {
 				System.out.println("Posição já usada, tente novamente.");
-				
-			}else { // jogada válida
-				jogoDaVelha[linha][coluna] = sinal;
-				jogada++;
+			}else { //jogadaValida
+				jogoVelha[linha][coluna] = sinal;
+				jogadas++;
 			}
 			
-			//Imprimir tabuleiro
-			for(int i = 0; i<jogoDaVelha.length; i++) {
-				for(int j = 0; j<jogoDaVelha[i].length; j++) {
-					System.out.print(jogoDaVelha[i][j] + " | ");
+			//imprimir tabuleiro
+			for (int i = 0; i<jogoVelha.length; i++) {
+				for (int j = 0; j<jogoVelha[i].length; j++) {
+					System.out.print(jogoVelha[i][j] + " | ");
 				}
 				System.out.println();
 			}
 			
-			//Verifica se tem ganhador
-			if ((jogoDaVelha[0][0] == 'X' && jogoDaVelha[0][1] == 'X' && jogoDaVelha[0][2] == 'X') || //linha 1
-					(jogoDaVelha[1][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][1] == 'X') || //linha 2
-					(jogoDaVelha[2][0] == 'X' && jogoDaVelha[2][1] == 'X' && jogoDaVelha[2][2] == 'X') || //linha 3
-					(jogoDaVelha[0][0] == 'X' && jogoDaVelha[1][0] == 'X' && jogoDaVelha[2][0] == 'X') || //coluna 1
-					(jogoDaVelha[0][1] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][1] == 'X') || //coluna 2
-					(jogoDaVelha[0][2] == 'X' && jogoDaVelha[1][2] == 'X' && jogoDaVelha[2][2] == 'X') || //coluna 3
-					(jogoDaVelha[0][0] == 'X' && jogoDaVelha[1][1] == 'X' && jogoDaVelha[2][2] == 'X')) { //diagonais
-			ganhou = true;
-				System.out.println("Jogador 1 venceu!");
-				
-			} else if ((jogoDaVelha[0][0] == 'O' && jogoDaVelha[0][1] == 'O' && jogoDaVelha[0][2] == 'O') || //linha 1
-			 		(jogoDaVelha[1][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][1] == 'O') || //linha 2
-					(jogoDaVelha[2][0] == 'O' && jogoDaVelha[2][1] == 'O' && jogoDaVelha[2][2] == 'O') || //linha 3
-					(jogoDaVelha[0][0] == 'O' && jogoDaVelha[1][0] == 'O' && jogoDaVelha[2][0] == 'O') || //coluna 1
-					(jogoDaVelha[0][1] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][1] == 'O') || //coluna 2
-					(jogoDaVelha[0][2] == 'O' && jogoDaVelha[1][2] == 'O' && jogoDaVelha[2][2] == 'O') || //coluna 3
-					(jogoDaVelha[0][0] == 'O' && jogoDaVelha[1][1] == 'O' && jogoDaVelha[2][2] == 'O')) { //diagonais
-			ganhou = true;
-				System.out.println("Jogador 2 venceu!");
-				
-			} else if (jogada > 9){
+			//verifica se tem ganhador
+			if ((jogoVelha[0][0] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][2] == 'X') || //linha 0
+				(jogoVelha[1][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[1][2] == 'X') ||	//linha 1
+				(jogoVelha[2][0] == 'X' && jogoVelha[2][1] == 'X' && jogoVelha[2][2] == 'X') ||	//linha 2
+				(jogoVelha[0][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][2] == 'X') || //diagonais
+				(jogoVelha[0][0] == 'X' && jogoVelha[1][0] == 'X' && jogoVelha[2][0] == 'X') || //coluna 0
+				(jogoVelha[0][1] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][1] == 'X') || //coluna 1
+				(jogoVelha[0][2] == 'X' && jogoVelha[1][2] == 'X' && jogoVelha[2][2] == 'X')) { //coluna 2
 				ganhou = true;
-				System.out.println("Ninguém ganhou essa partida.");
+				System.out.println("Parabéns, jogador 1 ganhou!");
+			} else 	if ((jogoVelha[0][0] == 'O' && jogoVelha[0][1] == 'O' && jogoVelha[0][2] == 'O') || //linha 0
+					(jogoVelha[1][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[1][2] == 'O') ||	//linha 1
+					(jogoVelha[2][0] == 'O' && jogoVelha[2][1] == 'O' && jogoVelha[2][2] == 'O') ||	//linha 2
+					(jogoVelha[0][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][2] == 'O') || //diagonais
+					(jogoVelha[0][0] == 'O' && jogoVelha[1][0] == 'O' && jogoVelha[2][0] == 'O') || //coluna 0
+					(jogoVelha[0][1] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][1] == 'O') || //coluna 1
+					(jogoVelha[0][2] == 'O' && jogoVelha[1][2] == 'O' && jogoVelha[2][2] == 'O')) { //coluna 2
+					ganhou = true;
+					System.out.println("Parabéns, jogador 2 ganhou!");
+			}else if (jogadas > 9) {
+					ganhou = true;
+					System.out.println("Ninguém ganhou essa partida.");
 			}
-		
 		}
-		
 	}
 }
