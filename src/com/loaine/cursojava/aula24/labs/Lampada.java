bc6330e536ea82e535932d7d344c09065992a654
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-09-22 20:24:16 -0300

    Created Exer01.java

---
+++ src/com/loaine/cursojava/aula24/labs/Lampada.java
@@ -0,0 +1,9 @@
+package com.loaine.cursojava.aula24.labs;
+
+public class Lampada {
+	String modelo, tensao, cor, tipoLuz;
+	double potencia;
+	int garantiaMeses;
+	String[] tipos;
+	boolean tipoAbajur;
+}
