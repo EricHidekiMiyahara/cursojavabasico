282a478b0e25c391682292457bccc267497493a6
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-10-05 12:11:48 -0300

    Informações adionadas

--- src/com/loaine/cursojava/aula27/labs/Aluno.java
+++ src/com/loaine/cursojava/aula27/labs/Aluno.java
@@ -1,22 +1,28 @@
 package com.loaine.cursojava.aula27.labs;
 
 public class Aluno {
-	
-	String nome;
-	String matricula;
-	String nomeCurso;
-	String[] nomeDisciplinas = new String[3];
-	double[][] notasDisciplinas = new double[3][4];
-	
-	void mostrarInfo() {
-		System.out.println("Nome: " + nome);
-		System.out.println("Matrícula: " + matricula);
-		System.out.println("Nome do curso: " + nomeCurso);
-		
-		for (int i = 0; i <notasDisciplinas.length; i++) {
-			
-		}
-	}
-	
-	
+    
+    String nome;
+    String matricula;
+    String nomeCurso;
+    String[] nomeDisciplinas = new String[3];
+    double[][] notasDisciplinas = new double[3][4];
+    
+    void mostrarInfo(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Nome do curso: " + nomeCurso);
+        
+        for (int i=0; i<notasDisciplinas.length; i++){
+            System.out.println("Notas da disciplina " + nomeDisciplinas[i]);
+            for (int j=0; j<notasDisciplinas[i].length; j++){
+                System.out.print(notasDisciplinas[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    
 }
+	
+	
