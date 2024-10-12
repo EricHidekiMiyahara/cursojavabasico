e006a50c0f2a7c0c127d177ee4a6db03e5e587d9
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-10-12 11:20:51 -0300

    Update Pacotes.java

--- src/com/loaine/cursojava/aula28/Pacotes.java
+++ src/com/loaine/cursojava/aula28/Pacotes.java
@@ -1,10 +1,23 @@
 package com.loaine.cursojava.aula28;
 
+import com.loaine.cursojava.aula20.labs.Exer04;
+import com.loaine.cursojava.aula20.labs.Exer05;
+import com.loaine.cursojava.aula20.labs.Exer06;
+import com.loaine.cursojava.aula27.Carro;
+
+
 public class Pacotes {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+	
+	Carro carro;
+	Carro carro2;
+	
+	Exer06 exer06;
+	Exer05 exer05;
+	Exer04 exer04;
+	
+	
     }
 
 }
